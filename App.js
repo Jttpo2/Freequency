bc23656aae1e312c4ccdb92743c663fe9ed9b2fc
John Petersson <johnpetersson@gmail.com>
2017-11-11 15:31:40 +0100

    Blinking styles.

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+
+    let pic = {
+      uri: 'https://cdn.pixabay.com/photo/2016/03/28/12/35/cat-1285634_1280.png'
+    };
     return (
-      <View style={styles.container}>
-        <Text>nnn</Text>
+      <View>
+      <Blink text='Blinky' /> 
       </View>
-    );
+      );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+class Fft extends Component {
+  render() {
+    return (
+      <Text> Fft for {this.props.day} here</Text>
+      );
+  }
+}
+
+class Blink extends Component {
+  constructor(props){
+    super(props);
+    this.state = {showText: true};
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return {
+          showText: !previousState.showText 
+        }
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text style={styles.bigRed}>{display}</Text>
+      );
+  }
+}
+
+const styles = StyleSheet.create( {
+  bigRed: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 30
+  }
 });
