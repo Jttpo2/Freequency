9674ae779cc7094bd3ae216162fc7abd3d3d8b7a
John Petersson <johnpetersson@gmail.com>
2017-11-13 12:06:13 +0100

    Indentation.

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, Button, Alert} from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
 
     let pic = {
       uri: 'https://cdn.pixabay.com/photo/2016/03/28/12/35/cat-1285634_1280.png'
     };
+
+    _onPressButton() {
+      Alert.alert('buttontap');
+    }
+
     return (
-      <View>
-      <Blink text='Blinky' /> 
+
+      <View style={styles.container}>
+
+        <View style={{
+          flex: 0.5,
+          flexDirection: 'column'
+        }}
+        >
+          <TextInput
+            style={{height: 40}}
+            placeholder='k'
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Text style={{fontSize: 33}}>
+            {this.state.text}
+          </Text>
+          <Button
+            onPress={this._onPressButton}
+            title='Tap'
+          />
+        </View>
+
       </View>
-      );
+    );
   }
 }
 
@@ -19,9 +49,18 @@ export default class App extends React.Component {
 class Fft extends Component {
   render() {
     return (
-      <Text> Fft for {this.props.day} here</Text>
-      );
-  }
+      // <Text> Fft for {this.props.day} here</Text>
+
+      <View style={{
+        flex: 0.5,
+        height: '50%'
+      }}>
+      <View style={{flex: 3, backgroundColor: 'skyblue'}} />
+    </View>
+
+
+  );
+}
 }
 
 class Blink extends Component {
@@ -32,7 +71,7 @@ class Blink extends Component {
     setInterval(() => {
       this.setState(previousState => {
         return {
-          showText: !previousState.showText 
+          showText: !previousState.showText
         }
       });
     }, 1000);
@@ -42,11 +81,17 @@ class Blink extends Component {
     let display = this.state.showText ? this.props.text : ' ';
     return (
       <Text style={styles.bigRed}>{display}</Text>
-      );
+    );
   }
 }
 
 const styles = StyleSheet.create( {
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   bigRed: {
     color: 'red',
     fontWeight: 'bold',
